f46312226e6a70e46754ab70114ffc2fadb2c046
truefalse <ifk.webdev@gmail.com>
2023-12-26 16:46:18 +0200

    comments

--- app/Http/Controllers/CountriesController.php
+++ app/Http/Controllers/CountriesController.php
@@ -9,30 +9,41 @@ use Illuminate\Validation\ValidationException;
 
 class CountriesController extends Controller
 {
+    // метод повернення країн
     public function getCountriesList(Request $request)
     {
+        // отримуємо країни з бази данних з сортуванням по `sort_order`
         $countries = Country::orderBy('sort_order')->get();
         
+        // виводимо отриманий лист у відповідь через ресурс `CountryResource`
         return CountryResource::collection($countries);
     }
 
+    // метод повернення країни по аліасу $country
     public function getCountry(Request $request, string $country)
     {
+        // отримуємо країну з бази данних по ключу `alias_name`, беремо единий кортеж first()
         $countryData = Country::where('alias_name', $country)->first();
 
+        // перевіряємо чи є такий кортеж, якщо ні виводимо виключення - Країну не знайдено
         if(empty($countryData))
             throw ValidationException::withMessages(['Країну не знайдено']);
 
+        // виводимо отриману країну у відповідь через ресурс `CountryResource`    
         return CountryResource::make($countryData);
     }
 
+    // метод повернення країни по аліасу міста $city
     public function getCity(Request $request, string $city)
     {
+        // отримуємо країну з бази данних по ключу `alias_capital`, беремо единий кортеж first()
         $countryData = Country::where('alias_capital', $city)->first();
 
+        // перевіряємо чи є такий кортеж, якщо ні виводимо виключення - Країну не знайдено
         if(empty($countryData))
             throw ValidationException::withMessages(['Країну не знайдено']);
 
+        // виводимо отриману країну у відповідь через ресурс `CountryResource`     
         return CountryResource::make($countryData);
     }
 }
--- app/Http/Resources/CountryResource.php
+++ app/Http/Resources/CountryResource.php
@@ -14,6 +14,7 @@ class CountryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        // формуємо поля обʼєкту країни для виводу у відповідь
         return [
             'id'        => $this->resource->id,
             'alias'     => $this->resource->alias_name,
